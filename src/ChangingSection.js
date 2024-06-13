6a2078f041b816252df4ed5a4f12e6d294391df7
Tabatalv <tabata.lopez11@gmail.com>
2024-06-13 20:31:06 +0200

    segundo commit

---
+++ src/ChangingSection.js
@@ -0,0 +1,26 @@
+
+
+export default [
+    {
+        h2: "Gots",
+        h3: "Global organic textile standard",
+        p: "Works behind the scenes, bringing about meaningful <br/> change from field to the certification label on your <br/> favorite products",
+        h4: "2023",
+        secondP: "· Website"
+},
+{
+    h2: "Room 8 Studio",
+    h3: "Game art production",
+    p: "Room 8 studio is a aaa art production studio from <br/> ukraine working with 7/10 of the top global publishers, <br/> including activision, nintendo, ubisoft, gameloft, ea, xbox game studios, and many others.",
+    h4: "2023",
+    secondP: "Corporate Website"
+},
+{
+    h2: "Prozorro.Sale",
+    h3: "Government auction system",
+    p: "Prozorro.sale is a govtech company that develops <br/> bespoke software solutions for the sale and lease of <br/> various state and municipal assets",
+    h4: "2023",
+    secondP: "UX/UI · WEB APP"
+
+}
+]
--- src/ThirdSection.jsx
+++ src/ThirdSection.jsx
@@ -3,37 +3,81 @@ import React, {useRef} from 'react'
 import Imagenes from './assets/Imagenes'
 import './ThirdSection.css'
 import {easeOut, motion, useMotionValueEvent, useScroll, useTransform} from 'framer-motion'
+import {useState} from 'react'
+import ChangingSection from './ChangingSection'
 
 function ThirdSection() {
-    const ref = useRef(null);
-    const {scrollYProgress} = useScroll({
-        target: ref
-        // offset: ["start start", "end end"]
-    });
+    // const ref = useRef(null);
+    const {scrollYProgress} = useScroll() 
+    const [changeBackground, setBackground] = useState(false)
+    const [cont, setCont] = useState(null)
+    
+    // ({
+    //     target: ref
+    //     // offset: ["start start", "end end"]
+    // });
     useMotionValueEvent(scrollYProgress, "change",
         (latest) => {
             console.log(latest)
+            if(latest > 0.80 && latest < 0.83){
+            setBackground(true)
+            setCont(0)
+            
         }
+        else if(latest > 0.83 && latest < 0.86){
+                setBackground(true);
+                setCont(cont + 1)
+        }
+        else if(latest > 0.86){
+            setBackground(true);
+                setCont(cont + 1)
+        }
+        // else{
+        //     setBackground(false)
+        // }
+        }
+        
     )
 
-    const color = useTransform(
-        scrollYProgress, 
-        [0, 0.9],
-        ["white", "red"]
-    )
+    // const color = useTransform(
+    //     scrollYProgress, 
+    //     [0, 0.6],
+    //     setBackground ? "red": "blue"
+    // )
   return (
-    <div ref={ref} className='changing-color container d-flex align-items-center justify-content-around'>
-        <div>
-            <motion.h2 style={{color}} className='cursive'>Gots</motion.h2>
-            <motion.h3 style={{color}} className='minus-cursive'>Global organic textile standard</motion.h3>
-            <motion.p style={{color}} className='not-cursive'>Works behind the scenes, bringing about meaningful <br/> change from field to the certification label 
-            on your <br/> favorite products</motion.p>
-            <h4 className='date'>2023</h4>
-            <p className='not-cursive'>· Website</p>
-        </div>
-        <img  src={Imagenes[0].ball}/>
+    <div>
+        {changeBackground ? (
+            
+            <div style={{backgroundColor: "#A0DCEE"}} className='changing-color container d-flex align-items-center justify-content-around'>
+                {console.log(ChangingSection[cont].h2)}
+                <div>
+                    <h2  className='cursive'>{ChangingSection[cont].h2}</h2>
+                    <h3 className='minus-cursive'>{ChangingSection[cont].h3}</h3>
+                    <p className='not-cursive'>{ChangingSection[cont].p}</p>
+                    <h4 className='date'>{ChangingSection[cont].h4}</h4>
+                    <p className='not-cursive'>{ChangingSection[cont].secondP}</p>
+                    </div>
+                    <img  src={Imagenes[0].ball}/>
+                    </div>
+   
+        ): (
+            <div className='changing-color container d-flex align-items-center justify-content-around'>
+                <div>
+                    <h2  className='cursive'>Gots</h2>
+                    <h3 className='minus-cursive'>Global organic textile standard</h3>
+                    <p className='not-cursive'>Works behind the scenes, bringing about meaningful <br/> change from field to the certification label 
+                    on your <br/> favorite products</p>
+                    <h4 className='date'>2023</h4>
+                    <p className='not-cursive'>· Website</p>
+                    </div>
+                    <img  src={Imagenes[0].ball}/>
+                    </div>
+
+        )
+        }
+        
     </div>
-  )
+  ) 
 }
 
 export default ThirdSection
